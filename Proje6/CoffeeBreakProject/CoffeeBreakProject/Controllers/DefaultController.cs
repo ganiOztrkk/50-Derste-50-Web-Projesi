ac2f8814ff1b7c71801927593a551d65f0456904
Gani Ozturk <ganioztrk@gmail.com>
2023-05-02 01:45:46 +0300

    proje6 - dinamik mvc self try

---
+++ Proje6/CoffeeBreakProject/CoffeeBreakProject/Controllers/DefaultController.cs
@@ -0,0 +1,71 @@
+﻿using CoffeeBreakProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeBreakProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCoffeeBreakEntities db = new DbCoffeeBreakEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialBanner()
+        {
+            var values = db.Headers.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialCopyRight()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialProducts()
+        {
+            var values = db.CoffeeProducts.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.AboutUs.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialCustomers()
+        {
+            var values = db.Customers.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Proje6/CoffeeBreakProject/CoffeeBreakProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoffeeBreakProject.MvcApplication" Language="C#" %>
