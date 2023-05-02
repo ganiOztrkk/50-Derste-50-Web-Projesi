efdd7b66e4aba688c0a0abc113a6959a65b36af3
Gani Ozturk <ganioztrk@gmail.com>
2023-05-02 17:40:42 +0300

    Proje7

---
+++ Proje7/DistinctProject/DistinctProject/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DistinctProject.Models.Entity;
+using DistinctProject.Models.ModelClass;
+
+namespace DistinctProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbDistinctEntities db = new DbDistinctEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            EnumarableClass cs = new EnumarableClass();
+            cs.Hakkimda = db.HAKKIMDAs.ToList();
+            cs.Iletisim = db.ILETISIMs.ToList();
+
+            return View(cs);
+        }
+    }
+}
---
+++ Proje7/DistinctProject/DistinctProject/Models/ModelClass/EnumarableClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DistinctProject.Models.Entity;
+
+namespace DistinctProject.Models.ModelClass
+{
+    public class EnumarableClass
+    {
+        public IEnumerable<HAKKIMDA> Hakkimda { get; set; }
+        public IEnumerable<ILETISIM> Iletisim { get; set; }
+    }
+}
