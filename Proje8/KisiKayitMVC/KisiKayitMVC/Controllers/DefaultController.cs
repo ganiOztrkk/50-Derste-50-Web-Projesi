b624071354e72f3e539b23c0766b63db13a99565
Gani Ozturk <ganioztrk@gmail.com>
2023-05-02 18:57:52 +0300

    proje9
    mvc ile insert işlemi

---
+++ Proje8/KisiKayitMVC/KisiKayitMVC/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KisiKayitMVC.Models.Entity;
+
+namespace KisiKayitMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        DbDistinctEntities db = new DbDistinctEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KisiEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KisiEkle(KISILER kisiler)
+        {
+            db.KISILERs.Add(kisiler);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
