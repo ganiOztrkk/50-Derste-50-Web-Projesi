08d05eaefff0d92e8fa63d3f9d2821ef834c7dba
Gani Ozturk <ganioztrk@gmail.com>
2023-05-04 00:32:16 +0300

    proje10

---
+++ Proje11/EmlakProje/EmlakProje/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmlakProje.Models.Entity;
+
+namespace EmlakProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbEmlakProjeEntities db = new DbEmlakProjeEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.EVLERs.ToList();
+            return View(degerler);
+        }
+    }
+}
