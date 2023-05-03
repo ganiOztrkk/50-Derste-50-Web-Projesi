d8e74081f1027ef5c9b92ca3aa00c92e6a0934f0
Gani Ozturk <ganioztrk@gmail.com>
2023-05-03 03:40:18 +0300

    proje9
    mvc html dropdownlist usage

---
+++ Proje9/DropdownListMVC/DropdownListMVC/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DropdownListMVC.Models.Entity;
+
+namespace DropdownListMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCitiesEntities db = new DbCitiesEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<SelectListItem> values = (from x in db.SEHIRLERs.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Value = x.ID.ToString(),
+                                                Text = x.SEHIR
+                                            }).ToList();
+            ViewBag.value = values;
+            return View();
+        }
+    }
+}
---
+++ Proje9/DropdownListMVC/DropdownListMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DropdownListMVC.MvcApplication" Language="C#" %>
